53e644f45a4fd7e7a321d5328a354a7436e8c09a
Hayden Caffey <hayden@Haydens-MacBook-Pro-2.local>
2023-08-03 22:01:15 -0400

    new branch for best practice, added shape logic

--- Main/utils/logoMaker.js
+++ Main/utils/logoMaker.js
@@ -3,11 +3,26 @@ const svg2img = require('svg2img');
 const fs = require('fs');
 
 // code informed by https://www.w3.org/TR/2003/REC-SVG11-20030114/shapes.html
-const svgTemplate = (text, textColor, shape, shapeColor) => `
-<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-  <rect width="100%" height="100%" fill="${shapeColor}" />
-  <text x="50%" y="50%" text-anchor="middle" fill="${textColor}" font-size="48">${text}</text>
-</svg>`;
+const svgTemplate = (text, textColor, shape, shapeColor) => {
+  let shapeElement;
+
+  switch (shape) {
+    case 'circle':
+      shapeElement = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+      break;
+    case 'triangle':
+      shapeElement = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`;
+      break;
+    case 'square':
+      shapeElement = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
+      break;
+  }
+  return  `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="white" />
+        ${shapeElement}
+        <text x="50%" y="50%" text-anchor="middle" fill="${textColor}" font-size="48">${text}</text>
+        </svg>`
+};
 
 //function to prompt choices w inquirer
 const makeLogo = async () => {
@@ -40,7 +55,7 @@ const makeLogo = async () => {
     fs.writeFileSync('logo.svg', svgContent, 'utf-8');
     console.log('Generated logo.svg');
     //using svg2img to translate template into png file of actual logo
-    svg2img('logo.svg', { width: 300, height: 200 }, (error, buffer) => {
+    svg2img('logo.svg', { width: 600, height: 400 }, (error, buffer) => {
       if (error) {
         console.error('Error converting SVG to PNG:', error);
       } else {
